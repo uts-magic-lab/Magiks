06a38fa466023461884dd94704375482ad0e967a
Nima Ramezani Taghiabadi <nima.ramezani@gmail.com>
2012-11-20 00:00:00 +1100

    pr2 kinematics v0.7

--- kinematics/special_geometries/pr2/PR2_kinematics.py
+++ kinematics/special_geometries/pr2/PR2_kinematics.py
@@ -14,8 +14,8 @@
                 Email(2): nima.ramezani@gmail.com
                 Email(3): nima_ramezani@yahoo.com
                 Email(4): ramezanitn@alum.sharif.edu
-@version:	    0.6
-Last Revision:  16 November 2012
+@version:	    0.7
+Last Revision:  20 November 2012
 References:     
                 [1] Jabref Key: 2008_arti_Shimizu.Kakuya.ea_Analyticala
 '''
@@ -97,40 +97,142 @@ def rot_z(q):
                         [  0,  0, 0, 1 ]])
 
 
+
+
+
 class PR2_ARM():
 
-    def end_right_arm_position(qr):
+            
+    def forward_update(self):        
+        '''
+        This function, finds all kinematics of the PR2 Arm directly from the formulations
+        '''    
 
-        s0 = math.sin(qr[0])
-        c0 = math.cos(qr[0])
-        s1 = math.sin(qr[1])
-        c1 = math.cos(qr[1])
-        s2 = math.sin(qr[2])
-        c2 = math.cos(qr[2])
-        s3 = math.sin(qr[3])
-        c3 = math.cos(qr[3])
+        self.c = [math.cos(self.qr[i]) for i in range(0,7)]
+        self.s = [math.sin(self.qr[i]) for i in range(0,7)]
 
-        s10    = s1*s0
-        c1     = math.cos(theta1)
-        c0s1   = c0*s1
-        c10    = c1*c0
-        c10s3  = c10*s3
+        [s0, s1, s2, s3, s4, s5, s6] = self.s
+        [c0, c1, c2, c3, c4, c5, c6] = self.c
+
+        [s10, s20, s30] = s0*numpy.array([s1, s2, s3])
+        [c10, c20, c30] = c0*numpy.array([c1, c2, c3])
+        
+        [c1s10, c1s20, c1s30] = c1*numpy.array([s10, s20, s30])
+
+        b = numpy.array([s1, c1])
+        [s21, c1s2] = s2*b
+        [c2s1, c21] = c2*b
+        [s31, c1s3] = s3*b
+        [c3s1, c31] = c3*b
+
+        [c21s0, c21s1, c21s2, c21s3] = c21*numpy.array([s0, s1, s2, s3])
+
+        [s31, s32]    = s3*numpy.array([s1, s2])
+        [c31, c32]    = c3*numpy.array([c1, c2])
+
+        [c0s0, c0s1, c0s2, c0s3, c210] = c0*numpy.array([s0, s1, s2, s3, c21])
+        [c0s30, c0s31, c0s32] = c0*numpy.array([s30, s31, s32])
+
+        b = numpy.array([c10, c20, c30])
+        [c10s0, c20s0, c30s0]  = s0*b
+        [c10s1, c20s1, c30s1]  = s1*b
+        [c10s2, c20s2, c30s2]  = s2*b
+        [c10s3, c20s3, c30s3]  = s3*b
+
         c30s1  = c30*s1
         c210s3 = c2*c10s3
-        s320   = s3*s2*s0
+        c21s30  = c21*s30
+        [c2s31, c2s0]  = c2*numpy.array([s31,s0])
+        s320   = s3*s20
+        c3s10  = c3*s10
 
-        k = k + 1
+
+        X =  c0*self.a0 + c0s1*self.d2 + (c30s1 + c210s3 - s320)*self.d4
+
+        Y =  s0*self.a0 + s10*self.d2 + (c3s10 + c0s32 + c21s30)*self.d4
+
+        Z =  c1*self.d2 + (c31 - c2s31)*self.d4
         
-        X =  c0*a0 + c0s1*d2 + (c30s1 + c210s3 - s320)*d4
+        self.end_right_arm_position = [X, Y, Z]
 
-        Y =  s0*a0 + s10*d2 + (c3s10 + c0s32 + c21s30)*d4
+         
+        R03 = numpy.zeros((3,3))      
 
-        Z =  c1*d2 + (c31 - c2s31)*d4
+        R03[0,0] =  -s20 + c210
+        R03[0,1] =  -c0s1
+        R03[0,2] =  -c2s0 - c10s2
+
+        R03[1,0] =  c0s2 + c21s0
+        R03[1,1] =  -s10
+        R03[1,2] =  c20 - c1s20
+
+        R03[2,0] =  -c2s1
+        R03[2,1] =  -c1
+        R03[2,2] =  s21
+
+        s54 = s5*s4
+        [c54, c5s4, c5s5] = c5*numpy.array([c4, s4, s5])
+
+        [s64, s65, c4s6, c5s6, c54s6, s654] = s6*numpy.array([s4, s5, c4, c5, c54, s54])
+        [c64, c65, c6s4, c6s5, c654, C6s54] = s6*numpy.array([s4, s5, c4, c5, c54, s54])
+
+        R47 = numpy.zeros((3,3))      
+
+        R47[0,0] =  -s64 + c654
+        R47[0,1] =  -c6s4 - c54s6
+        R47[0,2] =  c4*s5
+
+        R47[1,0] =  c4s6 + c65*s4
+        R47[1,1] =  c64 - c5*s64
+        R47[1,2] =  s54
+
+        R47[2,0] =  -c6s5
+        R47[2,1] =  s65
+        R47[2,2] =  c5
         
-        return [x, Y, Z]    
-            
+        R34 =  numpy.array([[  c3,     0,     s3 ],
+                            [  s3,     0,    -c3 ],
+                            [  0,      1,     0  ]])
 
-    def forward_update(self):        
+        self.end_right_arm_orientation = numpy.dot(numpy.dot(R03, R34), R47)
+
+        """
+        self.b = [s0, c0, c0s0]
+        '[s0, c0, c0s0] = b[0:3]'
+    
+        bb = s1*self.b
+        [s10, c0s1, c0s10] = bb
+        self.b = self.b + bb
+        #[s10, c0s1, c0s10] = b[3:6]
+
+        bb = c1*self.b
+        [c1s0, c10, c10s0, c1s10, c10s1, c10s10] = bb
+        self.b = self.b + bb
+        #[c1s0, c10, c10s0, c1s10, c10s1, c10s10] = b[6:12]
+
+        bb = s2*self.b
+        [s20, c0s2, c0s20, s210, c0s21, c0s210] = bb[0:6]
+        [c1s20, c10s2, c10s20, c1s210, c10s21, c10s210] = bb[6:12]
+        self.b = self.b + bb
+        #[s20, c0s2, c0s20, s210, c0s21, c0s210] = b[12:18]
+        #[c1s20, c10s2, c10s20, c1s210, c10s21, c10s210] = b[18:24]
+
+        bb = c2*self.b
+        [c2s0, c20, c20s0] = bb[0:3]
+        [c2s10, c20s1, c20s10] = bb[3:6]
+        [c21s0, c210, c210s0, c21s10, c210s1, c210s10] = bb[6:12]
+        [c2s20, c20s2, c20s20, c2s210, c20s21, c20s210] = bb[12:18]
+        [c21s20, c210s2, c210s20, c21s210, c210s21, c210s210] = bb[18:24]
+
+        self.b = self.b + bb
+        """
+
+
+
+
+
+
+    def forward_update_slow(self):        
         self.A = []
         '''        
         self.A.append(transfer_DH_standard( 0.0       , - math.pi/2, 0.0, 0.0       ,   self.qr[0]))
@@ -168,7 +270,7 @@ class PR2_ARM():
 
     def test_kinematic_equations(self): 
         
-        x_e = self.end_right_arm[0:3,3]
+        x_e = self.end_right_arm_position
 
         A = self.d2 + math.cos(self.qr[3])*self.d4
         B = self.d4*math.sin(self.qr[3])
@@ -244,7 +346,7 @@ class PR2_ARM():
         
         print 'I must be zero (9):', A*c1 - F*s1 - x_e[2]
 
-    def solve_inverse_kinematics_explained(self, T_d, tt0): 
+    def solve_inverse_kinematics_explained(self, position, orientation, tt0): 
      
         '''
         Finds all the solutions of the Inverse Kinematic problem for given phi
@@ -292,7 +394,7 @@ class PR2_ARM():
         v  = - (c + a*u)/b
         v2 = v**2
         A  = self.d2 + v*self.d4
-        print " I must be zero (0): ", v - math.cos(self.qr[3])
+        #print " I must be zero (0): ", v - math.cos(self.qr[3])
 
         i  = 0
         while i < 2:
@@ -320,7 +422,7 @@ class PR2_ARM():
                 In this case, a singularity happens
                 '''
             else: 
-                print " I must be zero (1): ", (gamma + alpha*v2 + betta*v)/(1 - v2) - (math.sin(self.qr[2])**2)
+                #print " I must be zero (1): ", (gamma + alpha*v2 + betta*v)/(1 - v2) - (math.sin(self.qr[2])**2)
                 w2  = (alpha*v2 + betta*v + gamma)/(1 - v2)
                 w   = math.sqrt(w2)
                 s2  = w
@@ -344,8 +446,8 @@ class PR2_ARM():
                         s1   = math.sin(theta1)
                         c1   = math.cos(theta1)
                         
-                        print " I must be zero (2): ", R1*math.cos(theta1 + sai1) - x_d[2] 
-                        print " I must be zero (3): ", A*c1 - F*s1 - x_d[2] 
+                        #print " I must be zero (2): ", R1*math.cos(theta1 + sai1) - x_d[2] 
+                        #print " I must be zero (3): ", A*c1 - F*s1 - x_d[2] 
 
                         As1Fc1 = self.a0 + A*s1 + F*c1
                         X      = c0*As1Fc1 - E*s0
@@ -399,12 +501,7 @@ class PR2_ARM():
 
     def __init__(self, a0 = 0.1, d2 = 0.4, d4 = 0.321):
 
-        self.qt = numpy.zeros((4))
-        self.qt[1] = drc*140.0    
-        self.qt[2] = drc*140.0    
-
         self.qr = numpy.zeros((7))
-        self.ql = numpy.zeros((7))
     
         self.a0 = a0
         self.d2 = d2
@@ -682,6 +779,19 @@ def symbolic_help(code):
         '''
 
         For R47 we have:
+
+        R47[0,0] =  -s4*s6 + c4*c5*c6
+        R47[0,1] =  -c6*s4 - c4*c5*s6
+        R47[0,2] =  c4*s5
+
+        R47[1,0] =  c4*s6 + c5*c6*s4
+        R47[1,1] =  c4*c6 - c5*s4*s6
+        R47[1,2] =  s4*s5
+
+        R47[2,0] =  -c6*s5
+        R47[2,1] =  s5*s6
+        R47[2,2] =  c5
+
         '''
         R47 = numpy.dot(numpy.dot(arm.R[4], arm.R[5]), arm.R[6])
 
@@ -699,31 +809,154 @@ def symbolic_help(code):
         print 'R47[2,2] = ', R47[2,2]
         print 
 
-
-
         """
-        '''
-        Now we simplify them:
-        '''
-        x76 = [simplify(x75[i]) for i in range(0,3)]
+        EQ1:  a*s0 + b*c3 + c = 0
+        EQ2:  alpha*c33 + betta*c3 + gamma - s22*s33 = 0
+        EQ3:  c1*d2 + (c31 - c2s31)*d4 - Z = 0
 
-        print 'X = ', x76[0]
-        print
-        print 'Y = ', x76[1]
-        print
-        print 'Z = ', x76[2]
+        EQ4: ax*(-s320 + c210s3 + c30s1) + ay*(c0s32 + c21s30 + c3s10) + az*(c31 - c2s31) -  c5 = 0
+        EQ5: ax*(-c2s0 - c10s2) + ay*(c20 - c1s20) + az*s21 - s54 = 0
+        EQ6: nx*(-s320 + c210s3 + c30s1) + ny*(c0s32 + c21s30 + c3s10) + nz*(c31 - c2s31) + c6s5 = 0
 
-        '''
-        Formulations are finally simplified to:
+        F10 =   a*c0
+        F13 = - b*s3
 
-        X =  c0*a0 + c0s1*d2 + (c30s1 + c210s3 - s320)*d4
-
-        Y =  s0*a0 + s10*d2 + (c3s10 + c0s32 + c21s30)*d4
-
-        Z =  c1*d2 + (c31 - c2s31)*d4
+        F22 = - 2*c2s332
+        F23 = - 2*alpha*c3s3 - betta*s3 - 2*c3s322
         
+        F31 = - d2*s1 - (c3s1 + c21s3)*d4
+        F32 =   s321*d4
+        F33 =   - (c1s3 + c32s1)*d4
+        
+        F40 = ax*(- c0s32 - c21s30 - c3s10) + ay*(- s320  + c210s3 + c30s1)  
+        F41 = ax*(- c20s31 + c310) + ay*(- c2s310 + c31s0) + az*(- c3s1 - c21*s3)
+        F42 = - ax*(c2s30 + c10s32) + ay*(c20s3 - c1s320) + az*s321
 
-        '''
+
+        F43 = ax*(- c3s20 + c3210 - c0s31) + ay*(c30s2 + c321s0 - s310) + az*(- c1s3 - c32s1)
+        F45 = s5
+
+        F50 = ax*(-c20 + c1s20) - ay*(c2s0 + c10s2)
+        F51 = ax*c0s21 - ay*s210 + az*c1s2
+        F52 = ax*(s20 - c210) - ay*(c0s2 + c21s0) + az*c2s1
+        F54 = - c4s5
+        F55 = - c5s4
+        
+        F60 = - nx*(c0s32 + c21s30 + c3s10) + ny*(- s320 + c210s3 + c30s1)
+        F61 =   nx*(- c20s31 + c310) + ny*(- c2s310 + c31s0) - nz*(c3s1 + c21s3)
+        F62 = - nx*(c2s30 + c10s32) + ny*(c20s3 - c1s320) - nz*s321
+        F63 =   nx*(- c3s20 + c3210 - c0s31) + ny*(c30s2 + c321s0 - s310) + nz*(- c1s3 - c32s1)
+        F65 =   c65
+        F66 = - s65
+
+        if F is a (7*7) matrix, we have: F*J.T = [1, 0, 0, 0, 0, 0, 0].T
+
+        J0 = 1
+
+        F10*J0 + F13*J3 = 0  ==>  J3 = - F10*J0/F13 = - F10/F13
+
+        F22*J2 + F23*J3 = 0  ==>  J2 = - F23*J3/F22
+
+        F31*J1 + F32*J2 + F33*J3 = 0 ==>  J1 = - (F32*J2 + F33*J3)/F31
+
+        F40*J0 + F41*J1 + F42*J2 + F43*J3 + F45*J5 = 0 ==> J5 = - (F40 + F41*J1 + F42*J2 + F43*J3)/F45
+
+        F50*J0 + F51*J1 + F52*J2 + F54*J4 + F55*J5 = 0 ==> J4 = - (F50 + F51*J1 + F52*J2 + F55*J5)/F54 
+
+        F60*J0 + F61*J1 + F62*J2 + F63*J3 + F64*J4 + F65*J5 + F66*J6 = 0  ==> J6 = - (F60 + F61*J1 + F62*J2 + F63*J3 + F64*J4 + F65*J5)/ J66
+
+
+
+        H100 = - a*s0
+
+        H133 = - b*c3
+
+        H222 = - 2*s33(- s22 + c22) = -2*(- s3322 + c22s33)
+        H223 = - 4*c2s2*c3s3 = -4*c32*s32
+
+        H232 = - 4*c3s3*s2c2 = -4*c32*s32
+        H233 = - 2*(c33 - s33)*(alpha + s22) - betta*c3 
+
+        H311 = - d2*c1 - (c31 - c2s31)*d4
+        H312 = - c1s32*d4
+        H313 = (- s31 + c321)*d4
+
+        H321 =   c1s32*d4
+        H322 =   c2s31*d4
+        H323 =   c3s21*d4
+
+        H331 =   (s1s3 - c32c1)*d4
+        H332 =   c3s21*d4
+        H333 =   (c2s31 - c31)*d4
+        
+        H400 = ax*(s320 - c210s3 - c30s1) - ay*(c0s32 + c21s30 + c3s10)  
+        H401 = ax*(c2s310 - c31s0) + ay*(c310 - c20s31)  
+        H402 = ax*(c1s320 - c20s3) - ay*(c2s30 + c10s32)  
+        H403 = ax*(s310 - c30s2 - c321s0) + ay*(c3210 - c3s20 - c0s31)  
+        H405 = 0
+
+        H410 =   ax*(c2s310 - c31s0) + ay*(c310 - c20s31)
+        H411 = - ax*(c210s3 + c30s1) - ay*(c21s30 + c3s10) + az*(c2s31 - c31)
+        H412 =   ax*c0s321 + ay*s3210 - az*c1s32
+        H413 = - ax*(c320s1 + c10s3) - ay*(c32s10 + c1s30) + az*(s31 - c321)
+        H415 = 0
+
+        H420  = ax*(- c20s3 + c1s320) + ay*(- c2s30 - c10s32)
+        H421  = ax*c0s321 + ay*s3210 + az*c1s32
+        H422  = ax*(s320 - c210s3) - ay*(c0s32 + c21s30) + az*c2s31
+        H423  = - ax*(c32s0 + c310s2) + ay*(c320 - c31s20) + az*c3s21
+
+        H430 =   ax*(s310 - c30s2 - c321s0) + ay*(c3210 - c3s20 - c0s31)
+        H431 = - ax*(c320s1 + c10s3) - ay*(c32s10 + c1s30) + az*(s31 - c321)
+        H432 = - ax*(c32s0 + c310s2) + ay*(c320 - c31s20) + az*c3s21
+        H433 =   ax*(s320 - c210s3 - c30s1) - ay*(c0s32 + c21s30 + c3s10) + az*(c2s31 - c31)
+
+        H455 = c5
+
+        H500 =   ax*(c2s0 + c10s2) - ay*(c20 - c1s20)
+        H501 = - ax*s210 + ay*c0s22
+        H502 =   ax*(c0s2 + c21s0) + ay*(s20 - c210)
+
+        H510 = - ax*s210 - ay*c0s21
+        H511 =   ax*c10s2 - ay*c1s20 - az*s21
+        H512 =   ax*c20s1 - ay*c2s10 + az*c21
+
+        H520 = ax*(c0s2 + c21s0) + ay*(s20 - c210)
+        H521 = ax*c20s1 + ay*c2s10 + az*c21
+        F522 = ax*(c2s0 + c10s2) - ay*(c20 - c1s20) - az*c2s1
+
+        H544 =   s54
+        H545 = - c54
+
+        H554 = - c54
+        H555 =   s54
+        
+        H600 = - nx*(c210s3 + c30s1 - s320) - ny*(c0s32 + c21s30 + c3s10)
+        H601 = - nx*(- c2s310 + c31s0) + ny*(- c20s31 + c310)
+        H602 = - nx*(c20s3 - c1s320) - ny*(c2s30 + c10s32)
+        H603 = - nx*(c30s2 + c321s0 - s310) + ny*(- c3s20 + c3210 - c0s31)
+
+        H610 =   nx*(c2s310 - c31s0) + ny*(c310- c20s31)
+        H611 = - nx*(c210s3 + c30s1) - ny*(c21s30 + c3s10) - nz*(c31 - c2s31)
+        H612 =   nx*c0s321 + ny*s3210 + nz*c1s32
+        H613 = - nx*(c320s1 + s3c10) - ny*(c32s10 + c1s30) - nz*(- s31 + c321)
+
+        H620 = - nx*(c20s3 - c1s320) - ny*(c2s30 + c10s32)
+        H621 =   nx*c0s321 - ny*s3210 - nz*s32c1 
+        H622 =   nx*(s320 - c210s3) - ny*(c0s32 + c21s30) - nz*c2s31
+        H623 = - nx*(c32s0 + c310s2) + ny*(c320 - c31s20) - nz*c3s21
+
+        H630 =   nx*(s310 - c30s2 - c321s0) + ny*(- c3s20 + c3210 - c0s31)
+        H631 = - nx*(c320s1 + c10s3) - ny*(c32s10 + c1s30) + nz*(s31 - c321)
+        H632 = - nx*(c32s0 + c310s2) + ny*(c320 - c31s20) + nz*c3s21
+        H633 =   nx*(s320 - c210s3) - ny*(s320 + s30c21) + nz*c2s31
+
+        H655 = - c6s5
+        F656 = - c5s6
+
+        F665 = - c5s6
+        F666 = - c6s5
+
         """
 
 
@@ -758,10 +991,10 @@ def main_2():
     print 'q1 = ', srs.qr
     
     srs.forward_update()
-    end_right_arm = numpy.copy(srs.end_right_arm)
+    era_pos = numpy.copy(srs.end_right_arm_position)
     srs.test_kinematic_equations()
 
-    all_solutions = srs.solve_inverse_kinematics_explained(srs.end_right_arm, tt0 = 45*drc)
+    all_solutions = srs.solve_inverse_kinematics_explained(srs.end_right_arm_position, srs.end_right_arm_orientation, tt0 = 45*drc)
 
     for i in range(0 ,len(all_solutions)):
         print all_solutions[i]
@@ -776,7 +1009,7 @@ def main_2():
         print 
         print end_right_arm
         '''
-        if vecmat.equal(srs.end_right_arm, end_right_arm):
+        if vecmat.equal(srs.end_right_arm_position, era_pos):
             print "Successful :-)"
         else:
             print "Failed :-("
