bc730335db7d997ffc9517e3871c66d676a6a5e2
Nima Ramezani <nima.ramezani@gmail.com>
2015-06-04 17:38:48 +1000

    added magiks/projects

---
+++ magiks/projects/s_pr2/__init__.py
@@ -0,0 +1,26 @@
+
+#Initialize Package: projects
+
+def set_file_path( verbose=False ):  
+    '''
+    execute the set_file_path function of the super package. 
+    -> an absolute path has to be set only at root level of magiks.
+    '''
+    import os 
+    import sys
+    
+    file_path   = os.path.abspath( os.path.dirname( __file__ ) )  
+    super_path  = os.path.abspath( os.path.join( file_path, '../..' ))
+    
+    if verbose :
+        print 
+        print file_path
+        print super_path
+        print 
+    
+    sys.path.append(  super_path )
+    
+    # super package : 
+    import projects
+    
+    return projects.set_file_path( verbose=verbose ) 
