354f201d3882b2b271df3d875d51860f739edba1
Nima Ramezani <nimasoft@nimasoft.pc>
2016-05-08 21:27:32 +1000

    main page markdown added

---
+++ search/pages_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['installing_20magiks',['Installing MAGIKS',['../md__home_nimasoft__dropbox_software_python_documentation_magiks_install_magiks.html',1,'']]]
+];
---
+++ search/pages_4.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['readme',['README',['../md__home_nimasoft__dropbox_software_python_documentation_s-pr2__r_e_a_d_m_e.html',1,'']]]
+];
